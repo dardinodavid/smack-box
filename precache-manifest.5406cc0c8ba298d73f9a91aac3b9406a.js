3a385cac8ed6ce9f546b6a61c256f1c64245d395
David Andrews <contactdardino@gmail.com>
2019-02-13 08:32:59 +0000

    Updates

---
+++ precache-manifest.5406cc0c8ba298d73f9a91aac3b9406a.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "39b97862de26aa915271ca9d0b6f2822",
+    "url": "/smack-box/static/media/hat-open1.39b97862.wav"
+  },
+  {
+    "revision": "e64ab7241911cd96a656",
+    "url": "/smack-box/static/css/main.a97c58e5.chunk.css"
+  },
+  {
+    "revision": "dc79c9601305ea8a33f4",
+    "url": "/smack-box/static/js/1.dc79c960.chunk.js"
+  },
+  {
+    "revision": "0af6b0254ba996684fed",
+    "url": "/smack-box/static/js/runtime~main.0af6b025.js"
+  },
+  {
+    "revision": "adaad39460344deee45a8ad09553bfde",
+    "url": "/smack-box/static/media/kick1.adaad394.wav"
+  },
+  {
+    "revision": "263dfcf8803977cc938496069150d4d4",
+    "url": "/smack-box/static/media/kick2.263dfcf8.wav"
+  },
+  {
+    "revision": "72315326ddf1c13f84027353e359567a",
+    "url": "/smack-box/static/media/kick3.72315326.wav"
+  },
+  {
+    "revision": "ccd61c08aed8e3adf16c29f631de1d87",
+    "url": "/smack-box/static/media/snare1.ccd61c08.wav"
+  },
+  {
+    "revision": "0431786efe276d99c4e2b04ca638fc3f",
+    "url": "/smack-box/static/media/snare2.0431786e.wav"
+  },
+  {
+    "revision": "ae960e7a5c12b6bfd9508264e46a9aa5",
+    "url": "/smack-box/static/media/snare3.ae960e7a.wav"
+  },
+  {
+    "revision": "956e83fe26ffaebd45e3e09d759286dc",
+    "url": "/smack-box/static/media/hat-closed.956e83fe.wav"
+  },
+  {
+    "revision": "e64ab7241911cd96a656",
+    "url": "/smack-box/static/js/main.e64ab724.chunk.js"
+  },
+  {
+    "revision": "c0d6d6918aeab5fbcb9a9ac194217b03",
+    "url": "/smack-box/static/media/hat-open2.c0d6d691.wav"
+  },
+  {
+    "revision": "779299a54d7078865111f105b2316f16",
+    "url": "/smack-box/static/media/kick1.779299a5.wav"
+  },
+  {
+    "revision": "d0fd7de40a8844c14f519258131c0feb",
+    "url": "/smack-box/static/media/kick2.d0fd7de4.wav"
+  },
+  {
+    "revision": "cf2415d510714d42f54d7273b4d28993",
+    "url": "/smack-box/static/media/kick3.cf2415d5.wav"
+  },
+  {
+    "revision": "849f74a5524b5a418683502fa033e374",
+    "url": "/smack-box/static/media/snare1.849f74a5.wav"
+  },
+  {
+    "revision": "569a9e24a1b540bee2f26f4a593bffc1",
+    "url": "/smack-box/static/media/snare2.569a9e24.wav"
+  },
+  {
+    "revision": "f78096beb9213ef05c186eeefe332e40",
+    "url": "/smack-box/static/media/snare3.f78096be.wav"
+  },
+  {
+    "revision": "43ed627439903d6ab3dc43b0a9b5cdd3",
+    "url": "/smack-box/static/media/hat-closed.43ed6274.wav"
+  },
+  {
+    "revision": "8825fa90a5b49c4da56782fbe815b622",
+    "url": "/smack-box/static/media/hat-open1.8825fa90.wav"
+  },
+  {
+    "revision": "968a79152f5275f3bfff139dd6e4f600",
+    "url": "/smack-box/static/media/hat-open2.968a7915.wav"
+  },
+  {
+    "revision": "891ac4d02ec4ff2112cad866000bb095",
+    "url": "/smack-box/index.html"
+  }
+];
